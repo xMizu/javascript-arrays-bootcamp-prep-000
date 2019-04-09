3b95b758d7ae0a13f60f0f13015bc602fa6b51b0
Anthony Wan <anthonywan00@gmail.com>
2019-04-09 01:05:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element)  
   return array
 }
+
+function addElementToBeginningOfArray (array, element) {
+  var aETBOF = [element,...array]
+  return aETBOF;
