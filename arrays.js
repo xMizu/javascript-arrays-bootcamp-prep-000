8e1f96a0697e32bb1f731499ef2b992f34ee6a7a
Anthony Wan <anthonywan00@gmail.com>
2019-04-09 00:57:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snicker', 'hundred grand','kitkat','skittles']
 
-function addElementToBeginningOfArray (array, element) {
+function addElementToBeginningOfArray (element, array) {
   array.unshift(element);
 } 
