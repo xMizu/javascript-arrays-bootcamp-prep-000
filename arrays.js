c6f7787785ecabb72d384a0c980c5d860346d7cc
Anthony Wan <anthonywan00@gmail.com>
2019-04-09 01:05:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,4 +5,7 @@ function addElementToBeginningOfArray (array, element) {
   return aETBOF;
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element)  
+  return array
+}
