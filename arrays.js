4a7fb12baf152e136029e256ca8ddcb6372a9e90
Anthony Wan <anthonywan00@gmail.com>
2019-04-09 01:09:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,6 @@ function destructivelyAddElementToEndOfArray (array, element) {
   return array
 }
 
-function accessElementInArray (array, element) {
-  
+function accessElementInArray (array, index) {
+  return array[index];
 }
